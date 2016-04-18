29045d6927f2949b40bb57e5b402673b98a71f22
jdanielcook <jdanielcook@gmail.com>
2016-04-18 18:13:47 -0400

    initial tool

---
+++ server.js
@@ -0,0 +1,114 @@
+/**
+ * Created by dcook on 4/18/16.
+ */
+
+"use strict";
+
+var express = require('express');
+var app = express();
+
+var validation_token = '9d5243867bf9eee4a8a907c14528e4e8';
+
+
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === validation_token) {
+        res.send(req.query['hub.challenge']);
+    }else {
+        res.send('Error, wrong validation token');
+    }
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+app.post('/webhook/', function (req, res) {
+    var messaging_events = req.body.entry[0].messaging;
+    for (var i = 0; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
+        if (event.message && event.message.text) {
+            var text = event.message.text;
+            // Handle a text message from this sender
+            console.log(text);
+        }
+    }
+    res.sendStatus(200);
+});
+
+var token = "<page_access_token>";
+
+function sendTextMessage(sender, text) {
+    var messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
+
+function sendGenericMessage(sender) {
+    var messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com/",
+                        "title": "Web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                },{
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
